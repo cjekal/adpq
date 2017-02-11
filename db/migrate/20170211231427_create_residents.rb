a25e420775477b8a955ae396dd1f153f19571de9
cjekal <charles.jekal@gmail.com>
2017-02-11 15:20:47 -0800

    updating schema

--- db/migrate/20170211231427_create_residents.rb
+++ db/migrate/20170211231427_create_residents.rb
@@ -1,10 +1,10 @@
 class CreateResidents < ActiveRecord::Migration[5.0]
   def change
     create_table :residents do |t|
-      t.string :email
+      t.string :email, null: false
       t.string :first_name
       t.string :last_name
-      t.string :zip_code
+      t.string :zip_code, null: false
       t.string :last_known_zip_code
 
       t.timestamps
--- db/migrate/20170211231529_create_events.rb
+++ db/migrate/20170211231529_create_events.rb
@@ -1,8 +1,8 @@
 class CreateEvents < ActiveRecord::Migration[5.0]
   def change
     create_table :events do |t|
-      t.string :category
-      t.string :unique_identifier
+      t.string :category, null: false
+      t.string :unique_identifier, null: false
       t.string :zip_code
       t.string :additional_info_url
 
