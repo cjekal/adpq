92216a252dfa709182bdfc4a5d989cd482f3a82a
cjekal <charles.jekal@gmail.com>
2017-02-11 16:02:40 -0800

    adding more Resident validation

--- app/models/resident.rb
+++ app/models/resident.rb
@@ -1,3 +1,4 @@
 class Resident < ApplicationRecord
   validates_uniqueness_of :email
+  validates_presence_of :email, :zip_code
 end
--- test/models/resident_test.rb
+++ test/models/resident_test.rb
@@ -10,5 +10,19 @@ class ResidentTest < ActiveSupport::TestCase
       other_resident.valid?
       other_resident.errors[:email].must_include('has already been taken')
     end
+
+    it 'should fail if email is blank' do
+      resident = residents(:valid)
+      resident.email = nil
+      resident.valid?
+      resident.errors[:email].must_include("can't be blank")
+    end
+
+    it 'should fail if zip_code is blank' do
+      resident = residents(:valid)
+      resident.zip_code = nil
+      resident.valid?
+      resident.errors[:zip_code].must_include("can't be blank")
+    end
   end
 end
