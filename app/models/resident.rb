5554f9a7c27b8e5a76ae1f9023634c3fcf9c2da9
cjekal <charles.jekal@gmail.com>
2017-02-11 16:00:04 -0800

    ensuring uniqueness of Resident#email

--- app/models/resident.rb
+++ app/models/resident.rb
@@ -1,2 +1,3 @@
 class Resident < ApplicationRecord
+  validates_uniqueness_of :email
 end
--- test/models/resident_test.rb
+++ test/models/resident_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class ResidentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  puts ENV['RAILS_ENV']
+
+  describe Resident do
+    it 'should fail if email is not unique' do
+      resident = residents(:valid)
+      other_resident = Resident.new(email: resident.email, zip_code: '12345')
+      other_resident.valid?
+      other_resident.errors[:email].must_include('has already been taken')
+    end
+  end
 end
