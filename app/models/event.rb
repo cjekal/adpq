ccfc35915f07ffd47136fbb9e9af03da3f8ac996
cjekal <charles.jekal@gmail.com>
2017-02-11 16:21:30 -0800

    adding event validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,4 @@
 class Event < ApplicationRecord
+  validates_presence_of :category, :unique_identifier
+  validates_uniqueness_of :unique_identifier, scope: :category
 end
--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  describe Event do
+    it 'should require a category' do
+      event = events(:valid_fire)
+      event.category = nil
+      event.valid?
+      event.errors[:category].must_include("can't be blank")
+    end
+
+    it 'should require a unique_identifier' do
+      event = events(:valid_fire)
+      event.unique_identifier = nil
+      event.valid?
+      event.errors[:unique_identifier].must_include("can't be blank")
+    end
+
+    it 'should require a unique unique_identifier for a given category' do
+      event = events(:valid_fire)
+      other_event = Event.new(category: event.category, unique_identifier: event.unique_identifier)
+      other_event.valid?
+      other_event.errors[:unique_identifier].must_include("has already been taken")
+    end
+  end
 end
